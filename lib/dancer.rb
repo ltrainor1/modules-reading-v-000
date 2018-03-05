dde22da69eef35e4c020c26b4494fc2c709fd5ca
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-05 21:51:50 +0000

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,15 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
+
+class Dancer
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+  extend Dance
+  extend MetaDancing 
+  
+  def initialize(name)
+    @name = name 
+  end 
+
+end
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,14 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
+
+class Kid
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+  extend Dance 
+  
+  def initialize(name)
+    @name = name 
+  end 
+ 
+end
