9fba7b5c1d081d8f13427bf6a4b04c41ab56e9eb
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-08 03:45:47 +0000

    automatically backed up by learn

--- lib/class_methods_module.rb
+++ lib/class_methods_module.rb
@@ -1,7 +1,10 @@
 module MetaDancing
   
-  def metadata
-    "This class produces objects that love to dance."
-end 
+#  def metadata
+#<<<<<<< HEAD
+#    "This class produces objects that love to dance."
+#=======
+#>>>>>>> 49e53e9b4ff601b24e5ac34e66d29309aa92bd8e
+#end 
   
 end 
